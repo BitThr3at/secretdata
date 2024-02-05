4a8ab91bdac9a2c6f695c0d638d7ab31cdd5802d
Naman <45028933+BitThr3at@users.noreply.github.com>
2024-02-05 13:36:13 +0530

    Update con.py

--- con.py
+++ con.py
@@ -3,7 +3,8 @@ import mysql.connector
 # Connect to the MySQL server
 cnx = mysql.connector.connect(
     user="root",
-    database="users"
+    database="users",
+    password="admin@Aw#$"
 )
 
 # Get the cursor
@@ -12,6 +13,8 @@ cursor = cnx.cursor()
 # Execute a sample query
 query = "SELECT * FROM table_name"
 cursor.execute(query)
+
+key=sk-6nUrA7vNuldOvXtwpblVT3BlbkAJ8Pki3T2TOuuRZw98HquA
 
 # Fetch all results
 results = cursor.fetchall()
